d30a505f82cec8fad0e220be71c5ec22cfcc3247
Radhika Ghali <59177211+R-code611@users.noreply.github.com>
2020-03-18 23:43:54 +0530

    Add files via upload

---
+++ splite_data.py
@@ -0,0 +1,3 @@
+#import tensorflow as tf
+import split_folders
+split_folders.ratio(r'C:\Users\DELL\Documents\Downloads\keras-yolo3-master\keras-yolo3-master\model_data\data',"save_folders",seed=3430,ratio=(.8,.2))
